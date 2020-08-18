619741494516253accbaf2159cbf2ef52df1b979
Mudesir Suleyman <nunasuleyman@gmail.com>
2020-08-18 00:51:51 -0700

    setup and basic flask app

---
+++ twitoff/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+from twitoff.routes.home_routes import home_routes
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(home_routes)
+    #app.register_blueprint(book_routes)
+    return app
+
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
