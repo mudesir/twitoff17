9adbc972518f0c8d00e69f8a0344f26dedf49469
Mudesir Suleyman <nunasuleyman@gmail.com>
2020-08-19 00:07:52 -0700

    models and migration

--- twitoff/__init__.py
+++ twitoff/__init__.py
@@ -1,12 +1,26 @@
 from flask import Flask
 
 
+from twitoff.models import db, migrate
 from twitoff.routes.home_routes import home_routes
 from twitoff.routes.book_routes import book_routes
 
 
+
+DATABASE_URL = "sqlite:///twitoff_99.db"
+#DATABASE_URL = "sqlite:////users/username/Desktop/your-repo-name/twitoff_99.db"
+#DATABASE_URL = "sqlite:///C:\\users\\username\\Desktop\\your-repo-name\\twitoff_99.db" 
+
+
+
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URL"] = DATABASE_URL
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
---
+++ twitoff/models.py
@@ -0,0 +1,24 @@
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+db = SQLAlchemy()
+
+migrate = Migrate()
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    author_id = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f"<Book {self.id} {self.title}>"
+
+def parse_records(database_records):
+    parsed_records = []
+    for record in database_records:
+        print(record)
+        parsed_record = record.__dict__
+        del parsed_record["_sa_instance_state"]
+        parsed_records.append(parsed_record)
+    return parsed_records
