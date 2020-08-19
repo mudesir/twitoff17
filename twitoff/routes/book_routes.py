67d0ad53e0c60b6f79ce2d7bcb4e0cf11596914b
Mudesir Suleyman <nunasuleyman@gmail.com>
2020-08-18 22:32:48 -0700

    new books navigation

--- twitoff/routes/book_routes.py
+++ twitoff/routes/book_routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify, request, render_template
+from flask import Blueprint, jsonify, request, render_template #, flash, redirect
 
 
 book_routes = Blueprint("book_routes", __name__)
@@ -22,4 +22,24 @@ def list_books_for_humans():
         {"id": 2, "title": "Book 2"},
         {"id": 3, "title": "Book 3"},
     ]
-    return render_template("books.html", message="Here's some books", books=books)
+    return render_template("books.html", message="Here's some books", books = books)
+
+
+
+@book_routes.route("/books/new")
+def new_book():
+    return render_template("new_book.html")
+
+
+@book_routes.route("/books/create", methods=["POST"])
+def create_book():
+    print("FROM DATA:", dict(request.form))
+    # todo
+    return jsonify({
+        "message": "BOOK CREATED OK (TODO)",
+        "book": dict(request.form)
+    })
+
+
+#flash(f"Book '{new_book.title}' created successfully!", "success")
+#return redirect(f"/books")
