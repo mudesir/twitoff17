c0040f719e598782ea5ecaa1f9d4ba885c43a45d
Mudesir Suleyman <nunasuleyman@gmail.com>
2020-08-19 14:13:58 -0700

    basilica service

--- twitoff/routes/services/basilica_service.py
+++ twitoff/routes/services/basilica_service.py
@@ -1,19 +1,27 @@
-#import basilica
+
 from basilica import Connection
+import osfrom dotenv import load_dotenv
 
+load_dotenv()
 
-API_KEY = "f696bca3-4682-5f8a-a807-99682d8c72f1"
+API_KEY = os.getenv("BASILICA_API_KEY")
 
+if __name__ == "__main__":
+
+connection = Connection(API_KEY)
+print("CONNECTION", type(connection))
 
 sentences= ["This is a sentenc", 
             "This is a similar sentence",
-            "I don't think this sentence is very similar at all..."]
+            "I don't think this sentence is very similar at all..."
+            ]
 
 
-connection = Connection(API_KEY)
+    embeddings = list(connection.embed_sentences(sentences))
+    print(embeddings)
 
 
-embeddings = list(connection.embed_sentences(sentences))
-
-
-print(embeddings)
+    embedding = connection.embed_sentence("Hello World!", model="twitter")
+    print(type(embedding))
+    print(type(embedding[0]))
+    print(len(embedding))
